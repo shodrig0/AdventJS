5a0adcf95fe1e6983bcb7eb684e94a50f8e1257d
shodrig0 <rodrialvillablanca@gmail.com>
2025-11-30 01:12:29 -0300

    04 creado

---
+++ src/04-createXmasTree.ts
@@ -0,0 +1,15 @@
+/**
+Crear una función que recibe la altura del árbol (un entero positivo entre 1 y 100) y un carácter especial para decorarlo.
+
+La función debe devolver un string que represente el árbol de Navidad, construido de la siguiente manera:
+
+1. El árbol está compuesto de triángulos de caracteres especiales.
+2. Los espacios en blanco a los lados del árbol se representan con guiones bajos _.
+3. Todos los árboles tienen un tronco de dos líneas, representado por el carácter #.
+4. El árbol siempre debe tener la misma longitud por cada lado.
+5. Debes asegurarte de que el árbol tenga la forma correcta usando saltos de línea \n para cada línea.
+ */
+
+function createXmasTree(height: number, ornament: string): string {
+    return ''
+}
